bb879827d29d2f3cf8fe590ded4da2abeb008ab3
hayate <hayate.k.0704@gmail.com>
2024-07-06 10:02:45 +0900

    update: 画面遷移できるようになった

---
+++ NavigationTest/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  NavigationTest
+//
+//  Created by 柿崎 on 2024/07/06.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NavigationTest/ViewController.swift
+++ NavigationTest/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    private let storyboardID = "Second"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func tappedNextButton(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "SecondViewController", bundle: nil)
+        guard let secondViewController = storyboard.instantiateViewController(withIdentifier: storyboardID) as? SecondViewController else {
+            print("ViewController with identifier 'SecondViewControllerID' not found")
+            return
+        }
+
+        guard let navigationController else { return }
+        navigationController.pushViewController(secondViewController, animated: true)
+    }
 }
 
